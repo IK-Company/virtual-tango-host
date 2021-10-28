cbc9beba18c351f87e6cda99d4c2fd2992e1b88e
ingvord <mail@ingvord.ru>
2021-10-28 10:55:51 +0200

    Ignore tests

--- src/test/java/com/github/ingvord/TestDatabase.java
+++ src/test/java/com/github/ingvord/TestDatabase.java
@@ -4,16 +4,19 @@ import fr.esrf.Tango.DevState;
 import fr.esrf.TangoApi.Database;
 import fr.esrf.TangoApi.DeviceProxy;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestDatabase {
 
     @Test
+    @Ignore
     public void testConnect() throws Exception{
         Database db = new Database("localhost",Integer.toString(10000));
     }
 
     @Test
+    @Ignore
     public void testConnectProxy() throws Exception{
         DeviceProxy proxy = new DeviceProxy("tango://localhost:10000/sys/database/2#dbase=no");
         Assert.assertSame(DevState.ON, proxy.state());
